a511134d2fe5c429841b18a1a93109e82c239a60
Mike <cmc8dm@virginia.edu>
2019-04-07 13:48:37 -0400

    props

--- mbapp/src/App.js
+++ mbapp/src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      results: []
+    };
+  }
+
   render() {
     return (
       <div className="App">
