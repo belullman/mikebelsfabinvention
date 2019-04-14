5e4991f9ff3a8f9ce213ecede5c2b396749ba886
Mike <cmc8dm@virginia.edu>
2019-04-14 13:30:12 -0400

    cardssssss

--- mbapp/src/FoodThemes/FoodThemes.js
+++ mbapp/src/FoodThemes/FoodThemes.js
@@ -1,35 +1,53 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
-import {Card, Button} from 'react-bootstrap';
-
-
+import {Card, CardColumns, Button} from 'react-bootstrap';
 class FoodThemes extends Component {
- 
-  render() {
-    return (
-      <div>
-        <div className="container" >
-          <div className="row"></div>
-            <Card bg="light" style={{ width: '18rem' }}>
-              <Card.Header>Header</Card.Header>
-              <Card.Body>
-                <Card.Title>Light Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk
-                  of the card's content.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-
-        
-
-
-
-    )
-  }
+render() {
+return (
+<div>
+   <div className="container" >
+      <div className="row"></div>
+      <CardColumns>
+         <Card border="primary" style={{ width: '18rem' }}>
+         <Card.Header>Header</Card.Header>
+         <Card.Body>
+            <Card.Title>Primary Card Title</Card.Title>
+            <Card.Text>
+               Some quick example text to build on the card title and make up the bulk
+               of the card's content.
+            </Card.Text>
+         </Card.Body>
+         </Card>
+         <br />
+         <Card border="secondary" style={{ width: '18rem' }}>
+         <Card.Header>Header</Card.Header>
+         <Card.Body>
+            <Card.Title>Secondary Card Title</Card.Title>
+            <Card.Text>
+               Some quick example text to build on the card title and make up the bulk
+               of the card's content.
+            </Card.Text>
+         </Card.Body>
+         </Card>
+         <br />
+         <Card border="success" style={{ width: '18rem' }}>
+         <Card.Header>Header</Card.Header>
+         <Card.Body>
+            <Card.Title>Success Card Title</Card.Title>
+            <Card.Text>
+               Some quick example text to build on the card title and make up the bulk
+               of the card's content.
+            </Card.Text>
+         </Card.Body>
+         </Card>
+         <br />
+      </CardColumns>
+      <br></br>
+      
+   </div>
+</div>
+)
 }
-
+}
 export default FoodThemes;
