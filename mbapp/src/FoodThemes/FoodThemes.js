f26202afdcbcb9ff7520456df05079b53ab3bc36
bel ullman <Bel@bels-MacBook-Air.local>
2019-04-14 13:42:03 -0400

    updated rows and columns cards

--- mbapp/src/FoodThemes/FoodThemes.js
+++ mbapp/src/FoodThemes/FoodThemes.js
@@ -43,6 +43,41 @@ return (
          </Card>
          <br />
       </CardColumns>
+      <CardColumns>
+         <Card border="primary" style={{ width: '18rem' }}>
+         <Card.Header>Header</Card.Header>
+         <Card.Body>
+            <Card.Title>Primary Card Title</Card.Title>
+            <Card.Text>
+               Some quick example text to build on the card title and make up the bulk
+               of the card's content.
+            </Card.Text>
+         </Card.Body>
+         </Card>
+         <br />
+         <Card border="secondary" style={{ width: '18rem' }}>
+         <Card.Header>Header</Card.Header>
+         <Card.Body>
+            <Card.Title>Secondary Card Title</Card.Title>
+            <Card.Text>
+               Some quick example text to build on the card title and make up the bulk
+               of the card's content.
+            </Card.Text>
+         </Card.Body>
+         </Card>
+         <br />
+         <Card border="success" style={{ width: '18rem' }}>
+         <Card.Header>Header</Card.Header>
+         <Card.Body>
+            <Card.Title>Success Card Title</Card.Title>
+            <Card.Text>
+               Some quick example text to build on the card title and make up the bulk
+               of the card's content.
+            </Card.Text>
+         </Card.Body>
+         </Card>
+         <br />
+      </CardColumns>
       <br></br>
       
    </div>
