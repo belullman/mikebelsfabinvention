2adf5514fc38e84b72341e08c759de3ca32eff67
Mike <cmc8dm@virginia.edu>
2019-04-17 20:46:51 -0400

    functionssssss

--- mbapp/src/App.js
+++ mbapp/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from "axios";
+
 import './App.css';
 import NavBar from './NavBar/NavBar';
 import FoodThemes from './FoodThemes/FoodThemes';
@@ -14,6 +16,29 @@ class App extends Component {
       results: []
     };
   }
+
+  getRecipes = () => {
+    // let catSearch = prompt("What do u wanna search, bich?");
+    axios
+      .get("https://images-api.nasa.gov/search?q=cat")
+      .then(response => { 
+        // console.log(response.data.collection.items[4].links[0].href)
+        if (this.state.user != null){
+          let pics =  response.data.collection.items
+          pics.forEach(pic => {
+            if (pic.links) {
+              this.addPic(pic.links[0].href)
+            }
+          });
+        }
+        console.log("it worked!!!")
+      })
+      .catch(error => { 
+        console.log(error);
+      });
+      alert("you are not logged in!")
+  };
+
   render() {
     return (
       <div>
